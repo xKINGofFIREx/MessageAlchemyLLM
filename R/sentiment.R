b6f641f918369a9bd5c5dcb06cb237ab2ac2e14e
xKINGofFIREx <artemilin270703@gmail.com>
2025-05-02 21:19:18 +0300

    ошибка с моделью была исправлена

--- R/sentiment.R
+++ R/sentiment.R
@@ -2,7 +2,7 @@ library(httr)
 
 token <- Sys.getenv("HF_TOKEN")
 headers <- add_headers(Authorization = paste("Bearer", token))
-url <- "https://api-inference.huggingface.co/models/blanchefort/rubert-base-cased-sentiment"
+url <- "https://api-inference.huggingface.co/models/cointegrated/rubert-tiny-sentiment-balanced"
 
 data <- readRDS("data/messages.rds")
 
@@ -10,8 +10,13 @@ analyze_sentiment <- function(text) {
   res <- POST(url, headers, body = list(inputs = text), encode = "json")
   out <- content(res)
   if (!is.null(out$error)) return("NEUTRAL")
-  best <- which.max(sapply(out, function(x) x$score))
-  out[[best]]$label
+  if (length(out) > 0) {
+    scores <- sapply(out[[1]], function(x) x$score)
+    best <- which.max(scores)
+    return(out[[1]][[best]]$label)
+  } else {
+    return("NEUTRAL")
+  }
 }
 
 data$sentiment <- sapply(data$text, analyze_sentiment)
